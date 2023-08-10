1aa5b3fa4cc6b51652b6a73ecd2e0dc6c29250b5
Chris Jones <ubercj@gmail.com>
2023-08-09 21:13:12 -0400

    migrate login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,22 @@
 	import { supabase } from '$lib/supabaseClient';
 	import { currentUser } from '$lib/stores/user.js';
 	import { onMount } from 'svelte';
+	import github_logo from '$lib/assets/github.svg';
 
 	/** @type {import('@supabase/supabase-js').AuthSession} */
 	let session;
+
+	/** @type {boolean}*/
+	let loading;
+
+	/** @type {string}*/
+	let signInEmail = '';
+
+	/** @type {string}*/
+	let signInPassword = '';
+
+	/** @type {string}*/
+	let errorMessage = '';
 
 	onMount(() => {
 		handleAuthState();
@@ -24,6 +37,45 @@
 			}
 		});
 	};
+
+	const handleSignInWithPassword = async () => {
+		try {
+			loading = true;
+			errorMessage = '';
+
+			const { data, error } = await supabase.auth.signInWithPassword({
+				email: signInEmail,
+				password: signInPassword
+			});
+
+			if (error) throw error;
+		} catch (error) {
+			console.error(error);
+			errorMessage = error.message;
+		} finally {
+			loading = false;
+		}
+	};
+
+	const handleSignInWithOAuth = async () => {
+		try {
+			loading = true;
+			errorMessage = '';
+
+			const { data, error } = await supabase.auth.signInWithOAuth({
+				provider: 'github',
+				options: {
+					redirectTo: import.meta.env.VITE_REDIRECT_URL
+				}
+			});
+
+			if (error) throw error;
+		} catch (error) {
+			console.error(error);
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
 <svelte:head>
@@ -31,12 +83,110 @@
 	<meta name="description" content="Create open source together" />
 </svelte:head>
 
-<section>
+<section class="login">
 	<h1>Ossum</h1>
+	<p class="subheading">Create open source together</p>
+	<div class="form-widget" aria-live="polite">
+		<sl-button
+			type="button"
+			class="social-button"
+			aria-live="polite"
+			{loading}
+			on:click={handleSignInWithOAuth}
+		>
+			<img slot="prefix" src={github_logo} alt="GitHub Icon" />
+			<span>Sign In with GitHub</span>
+		</sl-button>
+		<div role="separator" class="divider">
+			<sl-divider /><span>or</span><sl-divider />
+		</div>
+		<p class="description">Sign in using your email and password below</p>
+		<form class="form-widget" on:submit|preventDefault={handleSignInWithPassword}>
+			<sl-input
+				id="email"
+				label="Email"
+				type="email"
+				placeholder="Your email"
+				value={signInEmail}
+				on:sl-input={(e) => (signInEmail = e.target.value)}
+			/>
+			<sl-input
+				id="password"
+				label="Password"
+				type="password"
+				placeholder="Your password"
+				value={signInPassword}
+				on:sl-input={(e) => (signInPassword = e.target.value)}
+			/>
+			<sl-button type="submit" class="sign-in" aria-live="polite" {loading}>
+				<span>Submit Sign In</span>
+			</sl-button>
+			{#if errorMessage}
+				<p style="color: red;">{errorMessage}</p>
+			{/if}
+		</form>
 
-	<div>Create open source together</div>
+		<p class="description">
+			Don't have an account? Sign up <a href="/signup">here</a>.
+		</p>
+	</div>
 </section>
 
-<section>
-	<h2>Log In</h2>
-</section>
+<style>
+	.subheading {
+		text-align: center;
+	}
+
+	.description {
+		font-size: 1.1rem;
+	}
+
+	.form-widget {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		width: 100%;
+		max-width: 640px;
+		margin: auto;
+	}
+	.social-button::part(base) {
+		display: flex;
+		align-items: center;
+	}
+
+	.social-button::part(base):hover {
+		background-color: initial;
+	}
+
+	sl-input {
+		width: 100%;
+	}
+
+	sl-input::part(form-control) {
+		display: flex;
+		justify-content: flex-end;
+		width: 100%;
+	}
+
+	sl-input::part(form-control-label) {
+		display: flex;
+		align-items: center;
+		flex: 1 0 20%;
+	}
+
+	sl-input::part(form-control-input) {
+		flex: 1 0 80%;
+	}
+
+	.divider {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		gap: 1rem;
+	}
+
+	sl-divider {
+		flex: 1;
+		--width: 2px;
+	}
+</style>
