02c2dd3139295944c7e9e7a2486b2a45f9099054
Chris Jones <ubercj@gmail.com>
2023-09-03 17:07:35 -0400

    set up octokit

---
+++ src/lib/octokit.js
@@ -0,0 +1,33 @@
+import { PUBLIC_OCTOKIT_TOKEN } from '$env/static/public';
+import { Octokit } from '@octokit/core';
+
+/** @param {string} token */
+export const getUser = async (token) => {
+	const octokit = new Octokit({
+		auth: token
+	});
+
+	const user = await octokit.request('GET /user');
+	return user.data;
+};
+
+/** @param {string} username */
+export const getIssues = async (username) => {
+	const octokit = new Octokit({
+		auth: PUBLIC_OCTOKIT_TOKEN
+	});
+
+	const issues = await octokit.request('GET /search/issues', {
+		q: `type:pr author:${username} is:merged created:>=2023-01-01`
+	});
+	return issues.data.items.map(({ title, pull_request, created_at, closed_at, state, labels }) => {
+		return {
+			title,
+			url: pull_request?.url,
+			created_at,
+			closed_at,
+			state,
+			labels
+		};
+	});
+};
